b6239b123fa9611cfb9b15a5aa96bf8ba5778667
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-14 16:21:43 +0200

    [UPD] create different input type

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,49 @@ function App() {
 
   return (
     <>
-
+      <div className='row g-4'>
+        <div className="col-12">
+          <input 
+            type="text" 
+            placeholder='Autore'
+            className='form-control'
+            value=""
+          />
+        </div>
+        <div className="col-12">
+          <input 
+            type="text" 
+            placeholder='Titolo'
+            className='form-control'
+            value=""  
+          />
+        </div>
+        <div className='col-12'>
+          <textarea 
+            name="" 
+            id=""
+            className='form-control'
+            value=""
+          >
+          </textarea>
+        </div>
+        <div className="col-12">
+          <div className="form-check">
+            <input 
+              type="checkbox"
+              name="updates"
+              id="updates"
+              checked=""
+              className='form-check-input'
+            />
+          </div>
+        </div>
+          <div className='col-12'>
+            <button type="submit" className='btn btn-primary'>
+              Invia Post
+            </button>
+        </div>
+      </div>
     </>
   )
 }
