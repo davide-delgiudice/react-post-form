7db6d1983e2445b33f32bbaabc8602821d044f88
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-14 16:36:42 +0200

    [UPD] add container, h2 and form

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,58 @@ function App() {
 
   return (
     <>
-      <div className='row g-4'>
+    <div className="container-fluid mt-3">
+      <h2>Nuovo Post Form</h2>
+      <div className="row">
         <div className="col-12">
-          <input 
-            type="text" 
-            placeholder='Autore'
-            className='form-control'
-            value=""
-          />
-        </div>
-        <div className="col-12">
-          <input 
-            type="text" 
-            placeholder='Titolo'
-            className='form-control'
-            value=""  
-          />
-        </div>
-        <div className='col-12'>
-          <textarea 
-            name="" 
-            id=""
-            className='form-control'
-            value=""
-          >
-          </textarea>
-        </div>
-        <div className="col-12">
-          <div className="form-check">
-            <input 
-              type="checkbox"
-              name="updates"
-              id="updates"
-              checked=""
-              className='form-check-input'
-            />
-          </div>
-        </div>
-          <div className='col-12'>
-            <button type="submit" className='btn btn-primary'>
-              Invia Post
-            </button>
+          <form className='border rounded bg-light p-3'>
+            <div className='row g-4'>
+              <div className="col-12">
+                <input 
+                  type="text" 
+                  placeholder='Autore'
+                  className='form-control'
+                  value=""
+                />
+              </div>
+              <div className="col-12">
+                <input 
+                  type="text" 
+                  placeholder='Titolo'
+                  className='form-control'
+                  value=""  
+                />
+              </div>
+              <div className='col-12'>
+                <textarea 
+                  name="" 
+                  id=""
+                  className='form-control'
+                  value=""
+                >
+                </textarea>
+              </div>
+              <div className="col-12">
+                <div className="form-check">
+                  <input 
+                    type="checkbox"
+                    name="updates"
+                    id="updates"
+                    checked=""
+                    className='form-check-input'
+                  />
+                </div>
+              </div>
+                <div className='col-12'>
+                  <button type="submit" className='btn btn-primary'>
+                    Invia Post
+                  </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
+    </div>
     </>
   )
 }
